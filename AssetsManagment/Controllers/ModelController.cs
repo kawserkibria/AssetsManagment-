1521ee58cbc5dda03579aa204c95e0197655b7ab
kawserkibria <kawser.2000.dhaka@gmail.com>
2022-12-08 12:06:50 +0600

    Modal page add

---
+++ AssetsManagment/Controllers/ModelController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AssetsManagment.Controllers
+{
+    public class ModelController : Controller
+    {
+        //
+        // GET: /Model/
+        public ActionResult Index()
+        {
+            return PartialView("ModalPartialView");
+        }
+	}
+}
